c17f8879c755f2cbd65fca59f7e56071ef015180
Willem Veelenturf <wveelenturf@bol.com>
2018-04-25 14:03:49 +0200

    Small changes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var glob = require('glob');
+
 const cors = require('cors');
 const express = require('express');
 
@@ -8,6 +10,17 @@ const app = express();
 app.use(cors({
   origin: 'https://s.codepen.io'
 }));
+
+app.get('/', (req, res) =>{
+
+  glob('demo/*.html', function(err, files) {
+    console.log(files);
+    const list = files.map(file => `<li><a href="${file}">${file}</a></li>`)
+    const content = `<ul>${list.join('')}</ul>`
+    res.send(content)
+  });
+
+});
 
 app.use('/src', express.static('src'));
 app.use('/dist', express.static('dist'));
